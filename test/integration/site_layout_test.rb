9688fe9dae50d00bf7bad8282b7eda2e2286b34a
urassh <shuto.1011@icloud.com>
2023-04-28 00:43:20 +0000

    generate user controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,6 +13,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
   end
@@ -21,5 +22,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_equal "#{@base_title}", full_title
     assert_equal "Help | #{@base_title}", full_title("Help")
     assert_equal "About | #{@base_title}", full_title("About")
+    assert_equal "Sign up | #{@base_title}", full_title("Sign up")
   end
 end
